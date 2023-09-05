69ce20d9f8cf79ae82d93a7cdfaf92a5fae405d4
venugopal <bvenugopalreddy16@gmail.com>
2023-09-05 21:35:54 +0530

    migration file created

--- persons_app/models.py
+++ persons_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Persons(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    age = models.IntegerField(default=0)
