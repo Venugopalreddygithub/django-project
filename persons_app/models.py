b417a87ba9ae428c21f487e88856d603260cbaa2
venugopal <bvenugopalreddy16@gmail.com>
2023-09-06 21:04:35 +0530

    added admin.py

--- persons_app/models.py
+++ persons_app/models.py
@@ -5,3 +5,7 @@ class Persons(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=250)
     age = models.IntegerField(default=0)
+
+    class Meta:
+        #The name of the database for the model
+        db_table = "persons"
