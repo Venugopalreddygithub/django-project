2d4daea98a4ccf4ffd5b1d8ab864589680ab0f18
venugopal <bvenugopalreddy16@gmail.com>
2023-09-10 21:30:11 +0530

    mark todo as completed

--- todo_app/views.py
+++ todo_app/views.py
@@ -4,8 +4,45 @@ from todo_app.models import Todo
 # Create your views here.
 
 def index(request):
-    todos = Todo.objects.all()
+    all_todos = Todo.objects.all().order_by('-created_at')
     context = {
-        "todos": todos
+        "todos": all_todos
     }
     return render(request, 'index.html', context)
+
+def add_todo(request):
+    if request.method == "GET":
+        return HttpResponse('Invalid Method')
+    else:
+        todo_input = request.POST['todoInput']
+        new_todo = Todo(title=todo_input)
+        new_todo.save()
+        print(new_todo.id)
+        return redirect('todo_index')
+
+def detail_view(request, todoId):
+    a = Todo.objects.get(id=todoId)
+    context = {
+        'title': a.title,
+        'completed': a.completed,
+        'created_at': a.created_at,
+        'updated_at': a.updated_at
+    }
+    return JsonResponse(context)
+
+def delete_todo(request, todo_id):
+    if request.method == "GET":
+        return HttpResponse('Invalid Method')
+    else:
+        todo_object = Todo.objects.get(id=todo_id)
+        todo_object.delete()
+        return redirect('todo_index')
+
+def mark_todo(request, todo_id):
+    if request.method == "GET":
+        return HttpResponse('Invalid Method')
+    else:
+        todo_object = Todo.objects.get(id=todo_id)
+        todo_object.completed = True 
+        todo_object.save()
+        return redirect('todo_index')
